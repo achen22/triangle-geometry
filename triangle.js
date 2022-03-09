b340c93a0c8d6fcf082e4ba7d0b4e767201de091
Alan Chen <alanchen.2003@gmail.com>
2022-03-09 23:45:20 +1100

    Refactor: created class for triangle

--- triangle.js
+++ triangle.js
@@ -4,7 +4,7 @@ const TEST = true;
 const CSVREADER = new FileReader();
 const BUTTON = document.getElementById("button");
 const FILEINPUT = document.getElementById("file");
-/** @type {Point[][]} */
+/** @type {Triangle[]} */
 var triangles = [];
 
 /** Class representing a point in 2D space */
@@ -25,6 +25,7 @@ class Point {
     return this.x * this.x + this.y * this.y;
   }
 
+  /** @type {number} */
   get distance() { // from origin
     return Math.hypot(this.x, this.y);
   }
@@ -39,7 +40,77 @@ class Point {
    * @returns {Point} the relative position to this Point
    */
   to(other) {
-    return new Point(other.x - this.x, other.y - this.y);
+    return new Point(other.x - this.x, other.y - this.y, this.label + other.label);
+  }
+}
+
+/** Class representing three Points that form a triangle */
+class Triangle {
+  /**
+   * Create a triangle
+   * @param {Point[]} points array containing the three Points
+   */
+  constructor(...points) {
+    this.points = points.slice(0, 3);
+  }
+
+  get edges() {
+    let copy = this.points.slice();
+    let array = [];
+    for (let i = 0; i < 3; i++) {
+      array.push(copy[1].to(copy[2]));
+      copy.push(copy.shift()); // rotate
+    }
+    return array;
+  }
+
+  get angles() {
+    let copy = this.points.slice();
+    let array = [];
+    for (let i = 0; i < 3; i++) {
+      array.push(angle(...copy));
+      copy.push(copy.shift()); // rotate
+    }
+    return array;
+  }
+
+  get type() {
+    let label = "";
+    let square_distances = this.edges.map(edge => edge.sumOfSquares);
+    if (square_distances.some(d => d == 0)) {
+      // some points are the same
+      return "degenerate";
+    } else if (
+      // degenerate if slope is same for any two pairs
+      // m = (y1 - y0) / (x1 - x0) = (y2 - y0) / (x2 - x0)
+      // (y1 - y0) * (x2 - x0) = (y2 - y0) * (x1 - x0)
+      (this.points[1].y - this.points[0].y) * (this.points[2].x - this.points[0].x)
+      == (this.points[2].y - this.points[0].y) * (this.points[1].x - this.points[0].x)
+    ) {
+      // all points lie on the same line
+      return "degenerate";
+    } else if (square_distances[0] == square_distances[1] && square_distances[0] == square_distances[2]) {
+      return "equilateral";
+    } else {
+      let label = (square_distances[0] == square_distances[1] || square_distances[0] == square_distances[2] || square_distances[1] == square_distances[2])
+          ? " isoceles"
+          : " scalene";
+      
+      // diff = c^2 - a^2 - b^2 where c is longest side length
+      square_distances.sort((a, b) => a - b);
+      let diff = square_distances.pop();
+      square_distances.forEach(n => {
+        diff -= n;
+      });
+
+      if (diff == 0) {
+        return "right" + label;
+      } else if (diff > 0) {
+        return "obtuse" + label;
+      } else {
+        return "acute" + label;
+      }
+    }
   }
 }
 
@@ -70,96 +141,26 @@ function angle(a, b, c) {
 
 /**
  * Prints data about the triangle
- * @param {Point[]} triangle the triangle to describe
+ * @param {Triangle} triangle the triangle to describe
  * @returns {string} text about the triangle
  */
 function print(triangle) {
-  // list points
   let lines = [];
-  lines.push(triangle.map((point) => `${point.label}(${point.x}, ${point.x})`));
 
-  let square_distances = triangle.map(point => {
-    let points = triangle.filter(p => p != point);
-    return points[0].to(points[1]).sumOfSquares
+  // list points
+  lines.push(triangle.points.map((point) => `${point.label}(${point.x}, ${point.x})`).join(", "));
+
+  // list lengths
+  triangle.edges.forEach(edge => {
+    lines.push("Distance between " + edge.label.split("").join(" and ") + " is " + edge.distance);
   });
 
-  if (square_distances.some(d => d == 0)) {
-    // duplicate points
-    if (square_distances.every(d => d == 0)) {
-      lines.push("All points are the same!");
-    } else {
-      lines.push("Two points are the same!");
-      let squared = square_distances.reduce((a, b) => a + b) / 2; // sum all elements (0 + squared + squared)
-      lines.push("Distance between non-identical points is " + Math.sqrt(squared).toFixed(3));
-    }
-    lines.push("This is a degenerate triangle");
-
-  } else if (square_distances[0] == square_distances[1] && square_distances[0] == square_distances[2]) {
-    // equilateral triangle
-    lines.push("Distance between any two points is " + Math.sqrt(square_distances[0]).toFixed(3));
-    lines.push("This is an equilateral triangle");
-
-  } else if (square_distances[0] == square_distances[1] || square_distances[0] == square_distances[2] || square_distances[1] == square_distances[2]) {
-    // isoceles triangle
-    let i;
-    let others;
-    for (i = 0; i < 3; i++) {
-      let connected = square_distances.filter(d => d != square_distances[i]);
-      if (connected[0] == connected[1]) {
-        others = triangle.filter(p => p != triangle[i]);
-        lines.push("Distance between " + others.map(p => p.label).join(" and ") + " is " + Math.sqrt(square_distances[i]).toFixed(3));
-        others.map(p => p.label).forEach(l => {
-          lines.push("Distance between " + [triangle[i].label, l].join(" and ") + " is " + Math.sqrt(connected[0]).toFixed(3));
-        });
-        break;
-      }
-    }
-
-    // https://en.wikipedia.org/wiki/Isosceles_triangle
-    // only need to check one angle
-    let midpoint = new Point((others[0].x + others[1].x) / 2, (others[0].y + others[1].y) / 2);
-    if (triangle[i].x == midpoint.x && triangle[i].y == midpoint.y) {
-      lines.push("This is a degenerate triangle");
-    } else {
-      let squared = square_distances[i];
-      let diff = squared - square_distances.filter(d => d != squared).reduce((a, b) => a + b);
-      if (diff == 0) {
-        lines.push("This is a right isoceles triangle")
-      } else if (diff > 0) {
-        lines.push("This is an obtuse isoceles triangle")
-      } else {
-        lines.push("This is an acute isoceles triangle");
-      }
-    }
-
-  } else {
-    // scalene triangle
-    const largest = Math.max(...square_distances);
-    let diff = largest;
-    for (let i = 0; i < 3; i++) {
-      let labels = triangle.filter(p => p != triangle[i]).map(p => p.label);
-      lines.push("Distance between " + labels.join(" and ") + " is " + Math.sqrt(square_distances[i]).toFixed(3));
-      if (square_distances[i] != largest) {
-        diff -= square_distances[i];
-      }
-    }
-
-    // degenerate if slope is same for any two pairs
-    // m = (y1 - y0) / (x1 - x0) = (y2 - y0) / (x2 - x0)
-    // (y1 - y0) * (x2 - x0) = (y2 - y0) * (x1 - x0)
-    if (
-      (triangle[1].y - triangle[0].y) * (triangle[2].x - triangle[0].x)
-          == (triangle[2].y - triangle[0].y) * (triangle[1].x - triangle[0].x)
-    ) {
-      lines.push("This is a degenerate triangle");
-    } else if (diff == 0) {
-      lines.push("This is a right scalene triangle")
-    } else if (diff > 0) {
-      lines.push("This is an obtuse scalene triangle")
-    } else {
-      lines.push("This is an acute scalene triangle");
-    }
-  }
+  // classify triangle
+  let line = triangle.type;
+  line = (line[0].match(/[aeiou]/))
+      ? `This is an ${line} triangle`
+      : `This is a ${line} triangle`;
+  lines.push(line);
   
   return lines.join("<br>");
 }
@@ -212,11 +213,11 @@ CSVREADER.onload = function (event) {
         }
         return n;
       });
-      return [
+      return new Triangle(
         new Point(converted[0], converted[1], "A"),
         new Point(converted[2], converted[3], "B"),
         new Point(converted[4], converted[5], "C")
-      ];
+      );
     });
     display_triangles();
   } catch (error) {
@@ -264,6 +265,6 @@ if (TEST) {
   test = angle(...[[0,1], [1,1], [1,0]].map(p => new Point(...p)));
   console.assert(test == Math.PI / 4, `angle should be ${Math.PI / 4}`, test);
   test = angle(...[[0,Math.sqrt(3)], [1,0],[-1,0]].map(p => new Point(...p)));
-  // this test fails due to rounding errors :(
+  // this test fails due to rounding errors (tested on Firefox)
     console.assert(test == Math.PI / 3, `angle should be ${Math.PI / 3}`, test);
   }
